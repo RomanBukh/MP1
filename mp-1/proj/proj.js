650b1e5a2d348bacab1d402d2785bf1a37cf3446
RomanBukh <bukhovko@bu.edu>
2024-09-17 17:39:36 -0400

    Edit proj.js

--- mp-1/proj/proj.js
+++ mp-1/proj/proj.js
@@ -1,23 +1,3 @@
-function checkAge(){
-    let input=document.getElementById("input").value;
-    if(isNaN(input)){
-        document.getElementById("alt-output").style.display = "block";
-        document.getElementById("alt-output").innerHTML = `<p>You must enter a number</p>`; 
-    }else if(input <= 0){
-        document.getElementById("alt-output").style.display = "block";
-        document.getElementById("alt-output").innerHTML = `<p>Negative number is not acceptable</p>`; 
-    }else if(input%1 != 0){
-        document.getElementById("alt-output").style.display = "block";
-        document.getElementById("alt-output").innerHTML = `<p>The user has not entered a valid age</p>`; 
-    }else if(input >= 18 && input <=120){
-        document.getElementById("main-output").style.display = "block";
-    }else{
-        document.getElementById("alt-output").style.display = "block";
-        document.getElementById("alt-output").innerHTML = `<p>You may not see the content.</p>`;
-    }
-}
-
-
 function power(){
     let first=document.getElementById("first").value;
     first=Number(first);
