8d5f2d6946fc0a69dd19dd9b64124276f3977243
RomanBukh <bukhovko@bu.edu>
2024-09-17 17:47:50 -0400

    Clear function

--- mp-1/proj/proj.js
+++ mp-1/proj/proj.js
@@ -1,3 +1,35 @@
+function addition(){
+    let first=document.getElementById("first").value;
+    first=Number(first);
+
+    let second=document.getElementById("second").value;
+    second=Number(second);
+}
+
+function subtraction(){
+    let first=document.getElementById("first").value;
+    first=Number(first);
+
+    let second=document.getElementById("second").value;
+    second=Number(second);
+}
+
+function multiplication(){
+    let first=document.getElementById("first").value;
+    first=Number(first);
+
+    let second=document.getElementById("second").value;
+    second=Number(second);
+}
+
+function division(){
+    let first=document.getElementById("first").value;
+    first=Number(first);
+
+    let second=document.getElementById("second").value;
+    second=Number(second);
+}
+
 function power(){
     let first=document.getElementById("first").value;
     first=Number(first);
@@ -13,4 +45,10 @@ function power(){
     
     document.getElementById("output").innerHTML = `<p>${j}</p>`;
 
+}
+
+function clear(){
+    document.getElementById("first").innerHTML = '<p></p>';
+    document.getElementById("second").innerHTML = '<p></p>';
+    document.getElementById("output").innerHTML = '<p></p>';
 }
