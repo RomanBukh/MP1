f66e1c489079a150540cf9018d9a395bb4b93ff0
RomanBukh <bukhovko@bu.edu>
2024-09-17 17:56:04 -0400

    `` to ""

--- mp-1/proj/proj.js
+++ mp-1/proj/proj.js
@@ -60,7 +60,7 @@ function power(){
 }
 
 function clear(){
-    document.getElementById("first").innerHTML = ``;
-    document.getElementById("second").innerHTML = ``;
-    document.getElementById("output").innerHTML = ``;
+    document.getElementById("first").innerHTML = "";
+    document.getElementById("second").innerHTML = "";
+    document.getElementById("output").innerHTML = "";
 }
