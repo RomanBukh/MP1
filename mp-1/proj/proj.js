91b31be24b3e3a66e2ef00c0d0addb9b263218f1
RomanBukh <bukhovko@bu.edu>
2024-09-16 21:28:02 -0400

    Proj edits

---
+++ mp-1/proj/proj.js
@@ -0,0 +1,18 @@
+function checkAge(){
+    let input=document.getElementById("input").value;
+    if(isNaN(input)){
+        document.getElementById("alt-output").style.display = "block";
+        document.getElementById("alt-output").innerHTML = `<p>You must enter a number</p>`; 
+    }else if(input <= 0){
+        document.getElementById("alt-output").style.display = "block";
+        document.getElementById("alt-output").innerHTML = `<p>Negative number is not acceptable</p>`; 
+    }else if(input%1 != 0){
+        document.getElementById("alt-output").style.display = "block";
+        document.getElementById("alt-output").innerHTML = `<p>The user has not entered a valid age</p>`; 
+    }else if(input >= 18 && input <=120){
+        document.getElementById("main-output").style.display = "block";
+    }else{
+        document.getElementById("alt-output").style.display = "block";
+        document.getElementById("alt-output").innerHTML = `<p>You may not see the content.</p>`;
+    }
+}
